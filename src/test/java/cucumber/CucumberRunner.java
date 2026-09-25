65a6287db1025f4c08ffba571ae4e40e662106e0
Nassim <ounassim@gmail.com>
2026-09-25 22:49:07 +0200

    Add Cucumber runner class.

---
+++ src/test/java/cucumber/CucumberRunner.java
@@ -0,0 +1,11 @@
+package cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        glue = {"steps"}
+)
+public class CucumberRunner extends AbstractTestNGCucumberTests {
+}
