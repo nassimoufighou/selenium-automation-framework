9b2b6144f067435943d0ab9fb26c054d2fe61140
Nassim <ounassim@gmail.com>
2026-09-26 17:19:04 +0200

    Load URL from .properties file.

--- src/test/java/cucumber/CucumberHooks.java
+++ src/test/java/cucumber/CucumberHooks.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import utils.ConfigReader;
 
 public class CucumberHooks {
 
@@ -12,9 +13,10 @@ public class CucumberHooks {
 
     @Before
     public void before() {
+
         WebDriverManager.firefoxdriver().setup();
         webDriver = new FirefoxDriver();
-        webDriver.get("https://automationintesting.online/");
+        webDriver.get(ConfigReader.get("base.url", "config.properties"));
     }
 
     @After
